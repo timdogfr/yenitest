a1e26cb460cc3022c0a07b69183f18ae7d4ad14c
lesgotoken <104020078+lesgotoken@users.noreply.github.com>
2022-04-20 23:32:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,24 +25,17 @@ export const StyledRoundButton = styled.button`
   padding: 10px;
   border-radius: 100%;
   border: none;
-  background-color: var(--primary);
-  padding: 10px;
+  background-color: #747474;
+  padding: 19px;
   font-weight: bold;
   font-size: 15px;
-  color: var(--primary-text);
-  width: 30px;
-  height: 30px;
+  color: #191919;
+  width: 42px;
+  height: 18px;
   cursor: pointer;
   display: flex;
   align-items: center;
   justify-content: center;
-  box-shadow: 0px 4px 0px -2px rgba(250, 250, 250, 0.3);
-  -webkit-box-shadow: 0px 4px 0px -2px rgba(250, 250, 250, 0.3);
-  -moz-box-shadow: 0px 4px 0px -2px rgba(250, 250, 250, 0.3);
-  :active {
-    box-shadow: none;
-    -webkit-box-shadow: none;
-    -moz-box-shadow: none;
   }
 `;
 
