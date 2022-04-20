fe657440cade6bbc94a1c6d255114579ae5befb9
lesgotoken <104020078+lesgotoken@users.noreply.github.com>
2022-04-20 22:14:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,15 +16,8 @@ export const StyledButton = styled.button`
   padding: 10px;
   font-weight: bold;
   color: var(--secondary-text);
-  width: 100px;
+  width: 156px;
   cursor: pointer;
-  box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
-  -webkit-box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
-  -moz-box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
-  :active {
-    box-shadow: none;
-    -webkit-box-shadow: none;
-    -moz-box-shadow: none;
   }
 `;
 
