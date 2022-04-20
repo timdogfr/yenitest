c7f80432318912b2cdb15122c38905f9d0f13077
lesgotoken <104020078+lesgotoken@users.noreply.github.com>
2022-04-20 22:41:34 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -69,6 +69,7 @@ export const StyledLogo = styled.img`
 
 export const StyledImg = styled.img`
   box-shadow: 0px 5px 11px 2px rgba(0, 0, 0, 0.7);
+  border: 5px var(--secondary);
   background-color: var(--accent);
   border-radius: 100%;
   width: 368px;
