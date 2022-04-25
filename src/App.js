c7293fc63d0748e4e6f118730d41fec06d2a168c
timdogfr <103558704+timdogfr@users.noreply.github.com>
2022-04-25 23:42:06 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -249,13 +249,13 @@ function App() {
                   style={{ textAlign: "center", color: "var(--accent-text)" }}
                 >
                   1 {CONFIG.SYMBOL} costs {CONFIG.DISPLAY_COST}{" "}
-                  {CONFIG.NETWORK.SYMBOL}.
+                  {CONFIG.NETWORK.SYMBOL}
                 </s.TextTitle>
                 <s.SpacerXSmall />
                 <s.TextDescription
                   style={{ textAlign: "center", color: "var(--accent-text)" }}
                 >
-                  Excluding gas fees.
+                  Whitelist Mint
                 </s.TextDescription>
                 <s.SpacerSmall />
                 {blockchain.account === "" ||
