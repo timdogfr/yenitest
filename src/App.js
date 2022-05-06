01dc46798a81d23293a65b44f6d4a7da4fc29811
timdogfr <103558704+timdogfr@users.noreply.github.com>
2022-05-06 17:46:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -245,7 +245,7 @@ function App() {
                 color: "var(--accent-text)",
               }}
             >
-             <s.TextSubTitle color: "var(--accent-text)", size={5}>{1111-supply} of 1111 NFT's Available</s.TextSubTitle>
+             <s.TextSubTitle color: "var(--accent-text)">size={5}>{1111-supply} of 1111 NFT's Available</s.TextSubTitle>
             </s.TextTitle>
             <s.TextDescription
               style={{
