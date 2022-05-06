71d43f49fafad68c30c9a836acf384c707306272
timdogfr <103558704+timdogfr@users.noreply.github.com>
2022-05-06 17:08:03 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -89,6 +89,7 @@ function App() {
   const blockchain = useSelector((state) => state.blockchain);
   const data = useSelector((state) => state.data);
   const [claimingNft, setClaimingNft] = useState(false);
+  const [supply , setTotalSupply] = useState(0);
   const [feedback, setFeedback] = useState(`Click buy to mint your NFT.`);
   const [mintAmount, setMintAmount] = useState(1);
   const [CONFIG, SET_CONFIG] = useState({
